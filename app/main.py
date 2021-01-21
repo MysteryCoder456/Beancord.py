030b03514b3ee7ae4002c660af47e5285acd65af
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-21 14:27:33 +0400

    added simple window navigation and button

--- app/main.py
+++ app/main.py
@@ -1,23 +1,43 @@
 import socket
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-class LoginScreen(FloatLayout):
+class LoginWindow(Screen, FloatLayout):
     username_entry = ObjectProperty(None)
     ip_entry = ObjectProperty(None)
     port_entry = ObjectProperty(None)
 
+    def connect(self):
+        username = self.username_entry.text
+        ip_addr = self.ip_entry.text
+        port = self.port_entry.text
+
+        print(f"Connecting to {ip_addr} at port {port} as {username}...")
+
+
+class MainWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
 
 class Beancord(App):
     def build(self):
-        return LoginScreen()
+        wm.add_widget(LoginWindow(name='login'))
+        wm.add_widget(MainWindow(name='main'))
+        wm.current = "login"
+        return wm
 
 
 if __name__ == "__main__":
+    wm = WindowManager()
     Beancord().run()
 
     try:
