3f35c2a71889456a89c5e1268b21888f3be21656
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-01-21 18:02:23 +0400

    client connection to server

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 
+MSG_LENGTH = 2048
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+username = None
 
 
 class LoginWindow(Screen, FloatLayout):
@@ -13,11 +15,53 @@ class LoginWindow(Screen, FloatLayout):
     port_entry = ObjectProperty(None)
 
     def connect(self):
+        global username
+
         username = self.username_entry.text
         ip_addr = self.ip_entry.text
         port = self.port_entry.text
 
+        if username.strip() == "":
+            print("Please give a username!")
+            return
+
+        if ip_addr.strip() == "":
+            print("Please give an address!")
+            return
+
+        if port.strip() == "":
+            port = 8000
+
         print(f"Connecting to {ip_addr} at port {port} as {username}...")
+
+        s.settimeout(10)
+
+        try:
+            s.connect((ip_addr, int(port)))
+        except socket.gaierror:
+            print("Invalid address!")
+            return
+        except ValueError:
+            print("Invalid port!")
+            return
+        except ConnectionRefusedError:
+            print("Server not started!")
+            return
+        except socket.timeout:
+            print("Server took too long to respond!")
+            return
+
+        s.send(username.encode("utf8"))
+        response = s.recv(MSG_LENGTH).decode("utf8")
+
+        if response == "[DUPLICATE NAME]":
+            #TODO: Show this message on a label
+            print(f"The username {username} is already taken!")
+            return
+
+        if response == "[CONNECTION SUCCESS]":
+            print("Connection successfully established!")
+            self.manager.current = "main"
 
 
 class MainWindow(Screen):
@@ -30,6 +74,7 @@ class WindowManager(ScreenManager):
 
 class Beancord(App):
     def build(self):
+        wm = WindowManager()
         wm.add_widget(LoginWindow(name='login'))
         wm.add_widget(MainWindow(name='main'))
         wm.current = "login"
@@ -37,12 +82,11 @@ class Beancord(App):
 
 
 if __name__ == "__main__":
-    wm = WindowManager()
     Beancord().run()
 
     try:
-        s.send(b"QUIT")
+        s.send(f"[SENDER]{username}[SENDER]|[QUIT]".encode("utf8"))
     except Exception as e:
         print(e)
-
-    s.close()
+    finally:
+        s.close()
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import threading
 import socket
+import random
 import sqlite3
 
 ADDR, PORT = "0.0.0.0", 8000
@@ -8,23 +9,38 @@ s.bind((ADDR, PORT))
 s.listen()
 
 MSG_LENGTH = 2048
-clients = []
+clients = {}
+
+
+def generate_uuid():
+    while True:
+        uuid = ''.join([str(random.choice(range(10))) for i in range(5)])
+        if uuid not in clients.keys():
+            return uuid
 
 
 def accept_new_clients():
     while True:
         client_socket, addr = s.accept()
-
         new_client_username = client_socket.recv(MSG_LENGTH).decode("utf8")
-        clients.append(client_socket)
 
-        new_client_listen_thread = threading.Thread(target=listen_for_messages, args=(client_socket,), daemon=True)
+        if len([c for c in clients if c[1] == new_client_username]) > 0:
+            client_socket.send(b"[DUPLICATE NAME]")
+            client_socket.close()
+            continue
+
+        client_socket.send(b"[CONNECTION SUCCESS]")
+
+        new_uuid = generate_uuid()
+        clients[new_uuid] = (client_socket, new_client_username)
+
+        new_client_listen_thread = threading.Thread(target=listen_for_messages, args=(client_socket, new_uuid), daemon=True)
         new_client_listen_thread.start()
 
-        print(f"New Connection from {addr}.\nUsername: {new_client_username}")
+        print(f"New Connection from {addr}.\nUsername: {new_client_username}, UUID: {new_uuid}")
 
 
-def listen_for_messages(client_socket):
+def listen_for_messages(client_socket, uuid):
     while True:
         msg = client_socket.recv(MSG_LENGTH)
         msg_split = msg.decode("utf8").split("|", 1)
@@ -32,13 +48,19 @@ def listen_for_messages(client_socket):
 
         if msg_split[1] == "[QUIT]":
             client_socket.close()
+            del clients[uuid]
             print(f"{msg_sender} has left the chat...")
-        else:
-            msg_content = msg_split[1].strip("[CONTENT]")
-            print(f"{msg_sender} says: {msg_content}")
 
-        for c_sock in clients:
-            c_sock.send(msg)
+            for key in clients:
+                clients[key][0].send(msg)
+
+            return
+
+        msg_content = msg_split[1].strip("[CONTENT]")
+        print(f"{msg_sender} says: {msg_content}")
+
+        for key in clients:
+            clients[key][0].send(msg)
 
 
 def main():
